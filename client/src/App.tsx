47a0dd5ea72ad4e840b144c994fb0c004b05d37e
Tomás Julián Palasi <tomi.palasi@gmail.com>
2026-06-03 18:35:21 -0300

    Fix GitHub Pages routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,45 @@
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
-import ErrorBoundary from "./components/ErrorBoundary";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import WorkDetail from "./pages/WorkDetail";
+// import { Toaster } from "@/components/ui/sonner";
+// import { TooltipProvider } from "@/components/ui/tooltip";
+// import NotFound from "@/pages/NotFound";
+// import { Route, Switch } from "wouter";
+// import ErrorBoundary from "./components/ErrorBoundary";
+// import { ThemeProvider } from "./contexts/ThemeContext";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+// import WorkDetail from "./pages/WorkDetail";
 
-function Router() {
-  return (
-    <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/about"} component={About} />
-      <Route path={"/contact"} component={Contact} />
-      <Route path={"/work/:id"} component={WorkDetail} />
-      <Route path={"/404"} component={NotFound} />
-      {/* Final fallback route */}
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+// function Router() {
+//   return (
+//     <Switch>
+//       <Route path={"/"} component={Home} />
+//       <Route path={"/about"} component={About} />
+//       <Route path={"/contact"} component={Contact} />
+//       <Route path={"/work/:id"} component={WorkDetail} />
+//       <Route path={"/404"} component={NotFound} />
+//       {/* Final fallback route */}
+//       <Route component={NotFound} />
+//     </Switch>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <ErrorBoundary>
+//       <ThemeProvider defaultTheme="dark">
+//         <TooltipProvider>
+//           <Toaster />
+//           <Router />
+//         </TooltipProvider>
+//       </ThemeProvider>
+//     </ErrorBoundary>
+//   );
+// }
+
+// export default App;
 
 function App() {
-  return (
-    <ErrorBoundary>
-      <ThemeProvider defaultTheme="dark">
-        <TooltipProvider>
-          <Toaster />
-          <Router />
-        </TooltipProvider>
-      </ThemeProvider>
-    </ErrorBoundary>
-  );
+  return <h1>FUNCIONA</h1>;
 }
 
 export default App;
